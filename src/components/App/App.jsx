68a2afa69e717868767e9c4ca34e8a27741f3b3f
Ada Vuorinen <ada.vuorinen@gmail.com>
2024-12-24 16:40:25 +0200

    lisää AppRouter-komponentin ja reitityksen

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,29 +1,12 @@
-import styles from './App.module.scss'
-import { ButtonContainer } from '../../shared/buttons'
-import { FloatingButton } from '../../shared/buttons'
-import Content from '../Content'
-import Header from '../Header'
-import Items from '../Items'
-import Menu from '../Menu'
-import Settings from '../Settings'
-import Stats from '../Stats'
+import AppRouter from '../AppRouter'
 
 function App() {
 
   return (
     <>
-      <ButtonContainer>
-      <div className={styles.app}>
-        <Header />
-        <Content>
-          <Settings />
-        </Content>
-        <Menu />
-      </div>
-      </ButtonContainer>
+      <AppRouter />
     </>
   )
-
 }
 
 export default App
