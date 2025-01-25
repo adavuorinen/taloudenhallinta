a84475b42c1e3d391a7679be057ec839712441f6
Ada Vuorinen <ada.vuorinen@gmail.com>
2025-01-25 20:07:07 +0200

    lisää Firebase-sovelluksen alustuksen

--- src/components/ItemForm/ItemForm.jsx
+++ src/components/ItemForm/ItemForm.jsx
@@ -85,7 +85,7 @@ function ItemForm(props) {
                       disabled={values.type &&
                                 values.amount &&
                                 values.paymentDate &&
-                                values.receiver ? "" : "true"}
+                                values.receiver ? "" : "disabled"}
                       type='submit'>
                 { props.formData ? "TALLENNA" : "LISÄÄ" }
               </Button>
