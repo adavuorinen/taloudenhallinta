08abc83d7c9eec4bf063ee864c8ac4fe5047c224
Ada Vuorinen <ada.vuorinen@gmail.com>
2025-01-25 20:38:20 +0200

    lisää merkintöjen noudon Firestore-tietokannasta

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,26 @@
 import { useState } from 'react'
 import AppRouter from '../AppRouter'
 import useLocalStorage from '../../shared/uselocalstorage'
+import firebase from './firebase.js'
+import { collection, getFirestore, onSnapshot  } from 'firebase/firestore'
+import { useEffect } from 'react'
 
 function App() {
 
-  const [data, setData] = useLocalStorage('taloudenhallinta-data',[])
+  const [data, setData] = useState([])
   const [typelist, setTypelist] = useLocalStorage('taloudenhallinta-typelist',[])
+  const firestore = getFirestore(firebase)
+  
+  useEffect( () => {
+    const unsubscribe = onSnapshot(collection(firestore,'item'), snapshot => {
+      const newData = []
+      snapshot.forEach( doc => {
+        newData.push({ ...doc.data(), id: doc.id })
+      })
+      setData(newData)    
+    })
+    return unsubscribe
+  }, [])
 
   const handleItemDelete = (id) => {
     let copy = data.slice()
