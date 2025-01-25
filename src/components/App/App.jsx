d6ab25a6a5db56c32a5d786e54f36ba40022a067
Ada Vuorinen <ada.vuorinen@gmail.com>
2025-01-25 20:42:09 +0200

    lisää merkintöjen päivityksen ja poiston Firestore-tietokantaan

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import AppRouter from '../AppRouter'
 import useLocalStorage from '../../shared/uselocalstorage'
 import firebase from './firebase.js'
-import { collection, getFirestore, onSnapshot  } from 'firebase/firestore'
+import { collection, deleteDoc, doc, getFirestore, onSnapshot, setDoc  } from 'firebase/firestore'
 import { useEffect } from 'react'
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
   const [data, setData] = useState([])
   const [typelist, setTypelist] = useLocalStorage('taloudenhallinta-typelist',[])
   const firestore = getFirestore(firebase)
-  
+
   useEffect( () => {
     const unsubscribe = onSnapshot(collection(firestore,'item'), snapshot => {
       const newData = []
@@ -22,28 +22,12 @@ function App() {
     return unsubscribe
   }, [])
 
-  const handleItemDelete = (id) => {
-    let copy = data.slice()
-    copy = copy.filter(item => item.id !== id)
-    setData(copy)
+  const handleItemDelete = async (id) => {
+    await deleteDoc(doc(firestore, 'item', id))
   }
 
-  const handleItemSubmit = (newitem) => {
-    let copy = data.slice()
-
-    const index = copy.findIndex(item => item.id === newitem.id)
-    if (index >= 0) {
-      copy[index] = newitem
-    } else {
-      copy.push(newitem)
-    }
-
-    copy.sort( (a,b) => {
-      const aDate = new Date(a.paymentDate)
-      const bDate = new Date(b.paymentDate)
-      return bDate - aDate
-    })
-    setData(copy)
+  const handleItemSubmit = async (newitem) => {
+    await setDoc(doc(firestore, 'item', newitem.id), newitem)
   }
   
   const handleTypeSubmit = (type) => {
